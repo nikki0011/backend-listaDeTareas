f55cf488b94e226396e37b4178313949bbfe3d23
Nair Paez <nairpaez@outlook.com>
2026-06-19 11:28:34 -0300

    agregamos el modelo de tarea

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,3 +1,8 @@
  export const prueba = (req, res) =>{
     res.json('prueba de ruta, de nuevo')
 }
+
+
+ export const crearTarea = (req, res) =>{
+    res.json('hasta luego')
+}
---
+++ src/models/tarea.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+const TareaSchema = new Schema(
+    {
+        nombreTarea: {
+            type: String,
+            require: true,
+            unique : true,
+            minLength: 1,
+            maxLength: 100,
+            trim: true
+        }
+
+    }
+)
+
+const Tarea = mongoose.model('tarea',TareaSchema)
+
+export default Tarea
