9efab34bcbeb494311a1b503bbe28cc5823b2c1b
Nair Paez <nairpaez@outlook.com>
2026-06-28 16:43:33 -0300

    agregamos validaciones en rutas tareas

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,12 +1,13 @@
 import  { Router } from 'express'
 import { borrarTareaPorID, crearTarea, editarTareaPorID, listaTarea, obtenerTareaPorID, prueba } from '../controllers/tareas.controllers.js'
+import { validacionIDTarea, validacionTarea } from '../middlewares/validacionServicio.js'
 
 const router = Router()
 // http://localhost:3000/api/tareas/
 
 router.route('/test').get(prueba)
-router.route('/').post(crearTarea).get(listaTarea)
-router.route('/:id').get(obtenerTareaPorID).delete(borrarTareaPorID).put(editarTareaPorID).patch(editarTareaPorID)
+router.route('/').post(validacionTarea,crearTarea).get(listaTarea)
+router.route('/:id').get(validacionIDTarea,obtenerTareaPorID).delete(borrarTareaPorID).put([validacionIDTarea,validacionTarea],editarTareaPorID).patch(editarTareaPorID)
 
 
 export default router
